07dc2d70bfaee0220f8af0fc4480dfde997d3b4c
JavierPlata <jmplatap@academia.usbbog.edu.co>
2025-08-20 13:21:41 -0500

    Arreglar error en Extract

--- Extract/f1dbExtract.py
+++ Extract/f1dbExtract.py
@@ -6,8 +6,8 @@ class f1dbExtract:
     def __init__(self, csv_path):
         self.csv = csv_path
 
-def queries(self):
-    self.data = pd.read_csv(self.csv)
+    def queries(self):
+        self.data = pd.read_csv(self.csv)
 
-def response(self):
-    return self.data.head(5)
+    def response(self):
+        return self.data.head(5)
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from f1dbExtract import f1dbExtract
 
-response1 = f1dbExtract("F1Drivers_Dataset.csv")
+response1 = f1dbExtract("D:\Aplicaciones\Visual Studio Code\Proyectos Visual\F1DB\F1Drivers_Dataset.csv")
 
 response1.queries()
-response1.response()
 
 print(response1.response())
